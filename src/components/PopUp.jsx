49d3b99efeee9e7540ac1285800a42427389b1ce
avastravato <astravat@umich.edu>
2025-12-10 12:03:00 -0500

    style trial

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -2,13 +2,13 @@ import { X } from 'lucide-react';
 
 export const PopUp = () => {
     return (
-        <div>
-            <dialog open className='mx-auto animate-fade-in bg-primary w-150 h-75'>
+        <div className='rounded-lg'>
+            <dialog open className='mx-auto animate-fade-in bg-card w-150 h-75'>
                 <form method="dialog">
-                    <div className='bg-purple-200 w-150 p-1 flex justify-end'>
-                        <div className='w-8 h-8 p-1 bg-purple-50'>
+                    <div className='bg-card-header w-150 p-1 flex justify-end'>
+                        <div className='w-8 h-8 p-1'>
                             <div>
-                                <button><X /></button>
+                                <button><X className='text-primary-foreground'/></button>
                             </div>
                         </div>
                     </div>
