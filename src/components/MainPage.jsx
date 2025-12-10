614f86c121a307a7517f2d9cd997fade47c68e43
avastravato <astravat@umich.edu>
2025-12-10 13:00:50 -0500

    mainPage styling prog

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,8 @@
 export const MainPage = () => {
     return (
-        <div className='w-[50vw] aspect-[3/2] bg-card rounded-xl shadow-lg'>
-            <div className='w-full h-20 bg-card-header rounded-t-xl'>
-                <p className='text-white'>Users/avastravato/portfolio</p>
+        <div className='w-[50vw] aspect-[3/2] bg-card rounded-xl outline-4 outline-border'>
+            <div className='w-full h-20 bg-card-header rounded-t-xl flex items-center p-4 outline-4 outline-border'>
+                <p className='font-[Fira_Code] text-2xl text-white'>Users/avastravato/portfolio</p>
             </div>
         </div>
     );
